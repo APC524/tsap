8fe82a76dc0da0ea91cec61801aa32f05b9de4df
Zhuoran Yang <zy6@princeton.edu>
2016-12-13 04:52:55 -0500

    finish Gaussian mixture

--- src/cluster.py
+++ src/cluster.py
@@ -129,6 +129,43 @@ class Cluster(object):
             for i in range(nClusters):
                 Sigma[i] = cov_init
 
+            ll_old = 0
+            for i in range(max_iter):
+                exp_A = []
+                exp_B = []
+                ll_new = 0
+
+                # E-step
+                ws = np.zeros((k, n))
+                for j in range(k):
+                    ws[j, :] = pis[j] * mvn(mus[j], sigmas[j]).pdf(xs)
+                    ws /= ws.sum(0)
+
+                    # M-step
+                    pis = ws.sum(axis=1)
+                    pis /= n
+
+                    mus = np.dot(ws, xs)
+                    mus /= ws.sum(1)[:, None]
+
+                    sigmas = np.zeros((k, p, p))
+                    for j in range(k):
+                        ys = xs - mus[j, :]
+                        sigmas[j] = (ws[j,:,None,None] * mm(ys[:,:,None], ys[:,None,:])).sum(axis=0)
+                        sigmas /= ws.sum(axis=1)[:,None,None]
+
+                        # update complete log likelihoood
+                        ll_new = 0
+                        for pi, mu, sigma in zip(pis, mus, sigmas):
+                            ll_new += pi*mvn(mu, sigma).pdf(xs)
+                            ll_new = np.log(ll_new).sum()
+
+                            if np.abs(ll_new - ll_old) < tol:
+                                break
+                                ll_old = ll_new
+
+                                return ll_new, pis, mus, sigmas
+
         ###################################################################
         from scipy.spatial.distance import pdist, squareform
         import scipy.linalg as Linalg
