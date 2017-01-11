47a34a2fa998b6b584c7a0b75e13c645a05f02cc
Qingcan <qingcanw@princeton.edu>
2017-01-11 09:56:12 -0500

    Add line search in optimization methods

--- src/optim.py
+++ src/optim.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.optimize import line_search
 
 
 def sgd(w, dw, config=None):
@@ -9,6 +8,7 @@ def sgd(w, dw, config=None):
     config format:
     - learning_rate: Scalar learning rate.
     """
+
     if config is None: config = {}
     config.setdefault('learning_rate', 1e-2)
 
@@ -27,6 +27,7 @@ def sgd_momentum(w, dw, config=None):
     - velocity: A numpy array of the same shape as w and dw used to store a moving
       average of the gradients.
     """
+
     if config is None: config = {}
     config.setdefault('learning_rate', 1e-2)
     config.setdefault('momentum', 0.9)
@@ -46,13 +47,80 @@ def bfgs(x, dx, config):
 
     config format:
     - model: Model function.
-    - modelgrad: Model function gradient.
+    - modelgrad: The gradient of model function.
     - f: Function value at x.
-    - dx: Function gradient at x.
     - H: BFGS matrix.
     - alpha: Step size.
     """
-    def correction(H, s, y):
+
+    def golden_search(objfcn, a, x, b, fx, epsl=1e-6):
+        d = np.linalg.norm(a - b)
+        if d <= epsl:
+            return (a + b) / 2
+
+        TAU = 2 / (np.sqrt(5) - 1)
+        feval = int(np.ceil(np.log(d / epsl) / np.log(TAU))) 
+        tag = True
+        
+        for itr in range(feval):
+            y = a + b - x
+            fy = f(y)
+            if tag:
+                if fx >= fy:
+                    b = y
+                    tag = False
+                else:
+                    a, x, fx = x, y, fy
+            else:
+                if fx <= fy:
+                    a = y
+                    tag = True
+                else:
+                    b, x, fx = x, y, fy
+
+        return x
+    
+    def line_search(
+        objfcn, x0, d, fx0=None, alpha0=1e-3, epsl=1e-6, max_step=100):
+        if not fx0:
+            fx0 = objfcn(x0)
+
+        TAU = 2 / (np.sqrt(5) - 1)
+        d = alpha0 / np.linalg.norm(d) * d
+        x1 = x0 + d
+        fx1 = objfcn(x1)
+
+        if fx1 >= fx0:
+            d = -d / TAU
+            x2 = x0 + d
+            fx2 = objfcn(x2)
+            if fx2 >= fx0:
+                return golden_search(objfcn, x2, x0, x1, fx0, epsl)
+            else:
+                x1, fx1 = x2, fx2
+
+        for itr in range(5):
+            d = d * TAU
+            x2 = x1 + d
+            fx2 = objfcn(x2)
+            if fx2 >= fx1:
+                return golden_search(objfcn, x0, x1, x2, fx1, epsl)
+            x0, x1, fx1 = x1, x2, fx2
+
+        for itr in range(5, max_step):
+            x2 = x1 + d / TAU
+            fx2 = objfcn(x2)
+            if fx2 >= fx1:
+                return golden_search(objfcn, x2, x1, x0, fx1, epsl)
+            x0, x1, fx1 = x1, x2, fx2
+
+            x2 = x1 + d
+            fx2 = f(x2)
+            if fx2 >= fx1:
+                return golden_search(objfcn, x0, x1, x2, fx1, epsl)
+            x0, x1, fx1 = x1, x2, fx2
+
+    def update_H(H, s, y):
         u = np.vdot(y, s)
         v = np.dot(H, y)
         H += (np.dot(s, np.transpose(s)) * (1 + np.vdot(v, y) / u) / u
@@ -62,7 +130,6 @@ def bfgs(x, dx, config):
     objfcn = config['model']
     objfcngrad = config['modelgrad']
     config.setdefault('f', objfcn(x))
-    config.setdefault('dx', objfcngrad(x))
     config.setdefault('H', np.eye(x.size))
     config.setdefault('alpha', 1e-3)
     f = config['f']
@@ -70,14 +137,14 @@ def bfgs(x, dx, config):
     alpha = config['alpha']
 
     d = -np.linalg.solve(H, dx)
-    next_alpha = line_search(f=objfcn, myfprime=objfcngrad, xk=x, pk=d, gfk=dx,
-            old_fval=f)
+    next_x = line_search(
+        objfcn=objfcn, x0=x, d=d, fx0=f, alpha0=alpha, epsl=alpha*0.01)
     next_x = x + next_alpha * d
-    next_f, next_dx = objfcn(next_x)
-    next_H = correction(H, next_x - x, next_dx - dx)
+    next_f = objfcn(next_x)
+    next_H = update_H(H, next_x - x, next_dx - dx)
+    next_alpha = np.linalg.norm(next_x - x)
 
     config['f'] = next_f
-    config['dx'] = next_dx
     config['H'] = next_H
     config['alpha'] = next_alpha
     return next_x, config
